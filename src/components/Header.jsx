8613321a022917c7cb8bef109592a4aa0531e368
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-02-19 01:25:02 +0200

    login added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../img/logo.png';
 import Avatar from '../img/avatar.png';
-import {MdShoppingBasket} from "react-icons/md";
+import {MdShoppingBasket, MdAdd, MdLogout} from "react-icons/md";
 import {motion} from "framer-motion";
 import { Link } from 'react-router-dom';
 import { useStateValue} from "../context/StateProvider";
@@ -14,14 +14,22 @@ import {app} from "../firebase.config";
 const Header = () => {
 const firebaseAuth = getAuth(app);
 const provider= new GoogleAuthProvider();
-const[{user}, dispatch]=useStateValue();
 
-const login = async ()=> {
-  const {user : {refreshToken, ProviderData}} = await signInWithPopup(firebaseAuth, provider);
+const[{user}, dispatch]=useStateValue();
+const[isMenu, setIsMenu]=useState(false);
+const login = async () => {
+  if(!user){
+    const {
+      user : {refreshToken, ProviderData}
+    } = await signInWithPopup(firebaseAuth, provider);
   dispatch({
     type:actionType.SET_USER,
-    user: ProviderData[0]
-  })
+    user: ProviderData[0],
+  });
+  localStorage.setItem("user",JSON.stringify(ProviderData[0])); 
+  }else{
+    setIsMenu(!isMenu);
+  }
 };
 
   return (
@@ -54,13 +62,31 @@ const login = async ()=> {
            <div className="relative" >
            < motion.img 
               whileTap={{scale:0.5}}
-              src={Avatar} 
-              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-md cursor-pointer"
+              src={user ? user.photoURL:Avatar}  
+              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-md cursor-pointer rounded-full" 
               alt="userprofile"
               onClick={login}/>
+        {
+          isMenu && (  <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute 
+          top-12 right-0">
 
+        {
+          user&& user.email === "harunkoyuncu38@gmail.com"&&(
+            <Link to={'/create'}>
+            <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-00 
+          transition-all duration-100 ease-in-out text-textColor text-base">New Item<MdAdd /></p> 
+              </Link>
+          )
+        }
+       
+          <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
+          transition-all duration-100 ease-in-out text-textColor text-base">Logout<MdLogout /></p>
+      
+         
+          </div>)
+        }
            </div>
-          </div>
+          </div> 
           </div>
      {/*mobile*/}
         <div className="flex md:hidden w-full h-full" ></div>
