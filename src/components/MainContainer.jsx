d1a40099bd827d6655f49b3594f9f8feff6bebd1
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-02-20 00:53:10 +0200

    bike deliver logo and text added

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Delivery from '../img/delivery.png';
 
 const MainContainer = () => {
   return (
-    <div>MainContainer</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+      <div className="py-2 flex-1 flex-col items-start md:items-center">
+        <div className="flex items-center gap-2 justify-center bg-orange-100 p-2 rounded-full">
+          <p className="text-base text-orange-500 font-semibold">Bike Delivery</p>
+      <div className="w-10 h-10 rounded-full overflow-hidden bg-white">
+
+          <img src={Delivery}
+          className="w-full h-full object-contain"
+          alt="delivery"/>
+          </div>
+        </div>
+      </div>
+      <div className="py-2 bg-blue-400 flex-1"></div>
+    </div>
   )
 }
 
