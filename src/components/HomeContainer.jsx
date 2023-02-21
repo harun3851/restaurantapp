35c5d035755e4bf0f6a9204c8d15f30f97fd70f2
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-02-21 19:23:27 +0200

    data.js created for menu

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Delivery from '../img/delivery.png';
 import HeroBg from "../img/heroBg.png";
+
+import {heroData} from '../utils/data';
+
+
+
+
 const HomeContainer = () => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
     <div className="py-2 flex-1 gap-6 flex-col items-start justify-start md:items-center">
       <div className="flex items-center gap-2 justify-center bg-orange-100 p-2 rounded-full">
         <p className="text-base text-orange-500 font-semibold">Bike Delivery</p>
-    <div className="w-10 h-10 rounded-full overflow-hidden bg-white">
+         <div className="w-10 h-10 rounded-full overflow-hidden bg-white">
 
         <img src={Delivery}
         className="w-full h-full object-contain"
@@ -25,11 +31,29 @@ const HomeContainer = () => {
       </p>
 
       <button type="button"
-      className="bg-gradient-to-br from-orange-400 to-orange-500 w-full px-4 py-2 rounded-lg 
+      className="bg-gradient-to-br from-orange-400 to-orange-500 w-full px-4 py-2 rounded-3xl
       hover:shadow-lg transition-all ease-in-out duration-100 md:w-auto">order now</button> 
     </div>
-    <div className="py-2 flex-1 flex items-center">
-      <img src={HeroBg} className="ml top-16 right-16 h-685" alt="hero-bg"/>
+    <div className="py-2 flex-1 flex items-center relative">
+      <img src={HeroBg} className="ml-auto h-420 w-full " alt="hero-bg"/>
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center  py-4
+        gap-4 flex-wrap">
+         {heroData && 
+         heroData.map((n)=>(
+          <div 
+          key={n.id} 
+          className="w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center"> 
+          <img src={n.imageSrc} className="w-40 -mt-20 " alt="I1" />
+          <p className="text-base font-semibold text-textColor">{n.name}</p>
+          <p className="text-sm text-lighttextGray font-semibold my-3">
+            {n.desc}
+          </p>
+          <p className="text-sm text-lighttextGray font-semibold">
+            <span className="text-xs text-red-600">$</span>{n.price}
+          </p>
+        </div>
+         ))}
+      </div>
     </div>
   </section>
   );
---
+++ src/utils/data.js
@@ -0,0 +1,12 @@
+import I1 from "../img/i1.png";
+import F1 from "../img/f1.png";
+import C3 from "../img/c1.png";
+import Fi1 from "../img/fi1.png";
+
+export const heroData=[
+    
+    { id:1, name:'icecream', desc:'Chocolate&vanilla', price:'5.25', imageSrc: I1 },
+    { id:2, name:'fresh Strawberry', desc:'fresh', price:'10.25', imageSrc: F1 },
+    { id:3, name:'Mixed Kebab', desc:'mixed', price:'8.25', imageSrc: C3 },
+    { id:4, name:'chicken', desc:'fresh chicken', price:'6.25', imageSrc: Fi1 },
+]
