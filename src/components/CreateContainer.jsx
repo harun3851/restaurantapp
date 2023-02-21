52f1b4d8c91c0fa3cdd28f238e6ae94efba3eb79
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-02-21 23:59:51 +0200

    loader

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {motion} from "framer-motion";
 import {MdFastfood} from 'react-icons/md';
+import Loader from './Loader';
+import { categories } from '../utils/data';
+import { useState } from "react";
 
 const CreateContainer = () => {
 
@@ -12,7 +15,7 @@ const CreateContainer = () => {
   const [fields, setFields]=useState(false);
   const [alertStatus, setAlertStatus]=useState("danger");
   const [msg, setMsg]=useState(null);
-  const [isLoading, setIsloading]=useState(false);
+  const [isLoading, setIsloading]=useState(true);
 
 
   return (
@@ -32,10 +35,41 @@ const CreateContainer = () => {
       <div className="w-full py-2 border-b border-gray-300 flex
       items-center gap-2">
         <MdFastfood className="text-x1 text-gray-700"/>
-        <input type="text" required value={title} placeholder="give me a title"
+        <input 
+        type="text" 
+        required value={title}
+        onChange={(e)=> setTitle(e.target.value)}
+        placeholder="give me a title..."
         className="w-full h-full text-lg bg-transparent font-semibold
-        outline-none border-none placeholder:text-gray-400"></input>
+        outline-none border-none placeholder:text-gray-400"/>
       </div>
+      <div className="w-full">
+        <select onChange={(e)=> setCategory(e.target.value)}
+        className="outline-none w-full text-base border-b-2 border-gray-200 p-2 
+        rounded-md cursor-pointer ">
+          <option value="other" className="bg-white">
+            Select Catetory
+            </option>
+          {categories && categories.map((item)=>(
+            <option 
+            key={item.id} 
+            className="text-base border-0 
+            outline-none capitalize bg-white text-headingColor"
+            value={item.urlParamName}>
+              {item.name}
+            </option>
+          ))}
+
+        </select>
+      </div>
+      <div className="group flex justify-center items-center flex-col
+      border-2 border-dotted border-gray-300 w-full h-225 md:h-420
+      cursor-pointer rounded-lg ">
+         {isLoading ? <Loader />  : <>
+         {!imageAsset ? <>}
+         </>}
+      </div>
+     
      </div>
     </div>
    );
