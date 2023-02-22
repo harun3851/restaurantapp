89262f4b549aa4be63f43732d32787b5352b10af
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-02-22 14:58:16 +0200

    upload for image  option added

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {motion} from "framer-motion";
 import {MdFastfood} from 'react-icons/md';
+import {MdCloudUpload} from 'react-icons/md';
+import { MdDelete } from 'react-icons/md';
 import Loader from './Loader';
 import { categories } from '../utils/data';
 import { useState } from "react";
+import { upload } from '@testing-library/user-event/dist/upload';
 
 const CreateContainer = () => {
 
@@ -15,7 +18,9 @@ const CreateContainer = () => {
   const [fields, setFields]=useState(false);
   const [alertStatus, setAlertStatus]=useState("danger");
   const [msg, setMsg]=useState(null);
-  const [isLoading, setIsloading]=useState(true);
+  const [isLoading, setIsloading]=useState(false);
+  const deleteImage= () => {};
+  const uploadImage= () => {};
 
 
   return (
@@ -65,11 +70,49 @@ const CreateContainer = () => {
       <div className="group flex justify-center items-center flex-col
       border-2 border-dotted border-gray-300 w-full h-225 md:h-420
       cursor-pointer rounded-lg ">
-         {isLoading ? <Loader />  : <>
-         {!imageAsset ? <>}
-         </>}
+         {isLoading ? ( <Loader />
+         ):(  
+           <>
+           {!imageAsset ? (
+             <>
+           <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+            <div className="w-full h-full flex flex-col items-center justify-center gap-2">
+              <MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700" />
+              <p className="text-gray-500 hover:text-gray-700">
+                Click here to upload
+              </p>
+            </div>
+            <input 
+            type="file" 
+            name="uploadimage" 
+            accept="image/*" 
+            onChange={uploadImage} 
+            className="w-0 h-0"
+            />
+            </label>
+         </>
+         ):(
+          <>
+          <div className="relative h-full">
+            <img 
+            src={imageAsset} 
+            alt="uploaded image"
+            className="w-full h-full object-cover"
+            />
+            <button type="button" 
+            className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500
+            text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all
+            easy-in-out"
+            onClick={deleteImage}
+            >
+              <MdDelete className="text-white" />
+            </button>
+          </div>
+          </>
+         )}
+         </>
+         )}
       </div>
-     
      </div>
     </div>
    );
